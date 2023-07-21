cc86e578e799705ac83fdf4cd1fdadc1e0cc56a2
cyrus895 <cyrusghiai-chamlou@Cyruss-MacBook-Pro.local>
2023-07-21 14:18:14 -0700

    cc - description example slight tweak

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
@@ -31,7 +31,7 @@ public class UniversityController {
     @Operation(summary = "Get universities data list")
     @GetMapping("/get")
     public ResponseEntity<String> getUniversities(
-        @Parameter(name="name", description = "name to filter by, e.g. California", example="Stanford") @RequestParam String name
+        @Parameter(name="name", description = "name to filter by, e.g. Stanford", example="Stanford") @RequestParam String name
         )  throws JsonProcessingException {
         String result = universityQueryService.getJSON(name);
         return ResponseEntity.ok().body(result);
